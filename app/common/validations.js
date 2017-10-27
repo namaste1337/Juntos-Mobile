2baf7430800d9c1b602689cc3be55512125ffb1c
CheCm19 <che.cm@mail.com>
2017-10-27 00:34:41 -0700

    Adds comming file for string validations

---
+++ app/common/validations.js
@@ -0,0 +1,13 @@
+// This file will host helper function that will
+// validate strings
+
+
+
+// Handles email validation, return true if email is valid
+// false if the email is invalid
+export function validateEmail(email){
+	
+  var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(email);
+
+}
