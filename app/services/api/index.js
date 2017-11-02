0fde93eb189ab9fc23bcaf06af8a518873742093
CheCm19 <che.cm@mail.com>
2017-11-01 23:46:27 -0700

    Implements a promise to fetchAPI to be able to handle all error code alerts in a single place

--- app/services/api/index.js
+++ app/services/api/index.js
@@ -1,41 +1,60 @@
 import apiConfig from './config.js'
+import {basicAlert} from "./../../common/alerts"
 
 
 // Will be used to make all future API request, abtracts and configures.
-export default fetchApi = (endPoint, payload = {}, method = apiConfig.GET_METHOD, header = apiConfig.JSON_HEADER, params = {}) => {
-	
-  console.log(endPoint, payload, method, header)
+export default fetchAPI = (endPoint, payload = {}, method = apiConfig.GET_METHOD, header = apiConfig.JSON_HEADER, params = {}) => {
 
-	let headers = new Headers();
-	let url   	= apiConfig.URL + endPoint;
+  return new Promise(function(resolve, reject){
 
-  // Define accept header
-  headers.append(apiConfig.HEADER_ACCEPT, apiConfig.JSON_HEADER);
-  // Define send header
-	headers.append(apiConfig.HEADER_CONTENT_TYPE, header);
+    console.log(endPoint, payload, method, header)
+  
+    let headers = new Headers();
+    let url     = apiConfig.URL + endPoint;
+  
+    // Define accept header
+    headers.append(apiConfig.HEADER_ACCEPT, apiConfig.JSON_HEADER);
+    // Define send header
+    headers.append(apiConfig.HEADER_CONTENT_TYPE, header);
+  
+    var init = {   
+      method,
+      headers,
+    };
+  
+    // Body payload only allowed for POST, PUT, DELETE
+    if(method != apiConfig.GET_METHOD){
 
-	var init = {   
-    method,
-    headers,
-  };
+      if(header == apiConfig.JSON_HEADER){
+        init.body = JSON.stringify(payload)
+      }else if(header == apiConfig.MULTI_PART_HEADER){
+        init.body = payload;
+      }
 
-  // Body payload only allowed for POST, PUT, DELETE
-  if(method != apiConfig.GET_METHOD){
-    if(header == apiConfig.JSON_HEADER){
-      init.body = JSON.stringify(payload)
-    }else if(header == apiConfig.MULTI_PART_HEADER){
-      init.body = payload;
     }
-    
-  }
 
-  console.log(init);
+    fetch(url, init).then(response => {
 
-	return fetch(url, init).then(response => {
-      return response.json();
-  }).catch(error => {
-      return error;
+      // Round the status code;
+      let statusCode    = Math.floor(response.status/100);
+      
+      // Handle the response
+      response.json().then(function(body){
+        if(statusCode === 2){
+          resolve(body)
+        }else{
+          throw Error(body.data.message);
+        }
+      }).catch(error =>{
+        // Display any application level error in a alert
+        basicAlert("", error.toString());
+        // Pass application level errors 
+        reject(error);
+      });
+    }).catch(error => {
+      // Log network level errors
+      console.log(error);
+    });
   });
-
 
 }
