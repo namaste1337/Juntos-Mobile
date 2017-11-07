ec158c657980fc3fa7f603bee9d0685eb6c4ec77
CheCm19 <che.cm@mail.com>
2017-11-06 22:08:45 -0800

    Implements landing screen logo

--- app/screens/Landing.js
+++ app/screens/Landing.js
@@ -1,17 +1,62 @@
+//////////////////////////////
+// Imports
+///////////////////////////////
+
 import React, {Component} from 'react';
-import {Text, View, Button} from 'react-native';
+import {
+  StyleSheet,
+  View, 
+  Button,
+  Image,
+} 
+from 'react-native';
+
+
+//////////////////////////////
+// Imports Common Files
+///////////////////////////////
+
+// Common styles
+import CommonStyles from "../common/styles.js";
+
+////////////////////////
+// Constants
+////////////////////////
+
+// Images
+const logo = require("./../assets/login/mobile_logo.png");
+// Properties 
+const LOGO_IMAGE_RESIZE_MODE_PROPERTY = "contain";
 
 class LandingView extends Component {
-	 static navigationOptions = {
-    	title: 'Welcome',
-  	}
      render(){
-          return (
-               <Button
-                    onPress={() => this.props.navigation.navigate('Login', {name: 'Landing'})}
-                    title="Go to Login"/>
-               );
-          }
+      return (
+        <View style={CommonStyles.container}>
+          <View style={styles.logoWrapper}>
+            <Image style={styles.logoImage} source={logo} resizeMode={LOGO_IMAGE_RESIZE_MODE_PROPERTY}/>
+          </View>
+        </View>
+      );
+    }
 };
+
+
+////////////////////////
+// Screen Styles
+////////////////////////
+
+const styles = StyleSheet.create({
+
+  logoWrapper:{
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  logoImage:{
+    height: 200,
+    width: 200
+  }
+
+});
 
 export default LandingView;
