a5023db1a70aa9cc0011d9c91bba69753e1524da
CheCm19 <che.cm@mail.com>
2017-11-10 17:59:19 -0800

    Improves comments

--- app/common/device.js
+++ app/common/device.js
@@ -1,7 +1,10 @@
 // This file will host constants and functions
-// related to device propertes ex. device types,
+// related to device properties ex. device types,
 // device width and height.
 
+// The following constant define the device strings
+// that may be shared through out the app to check
+// the users device type ex. Platform.OS == deviceTypes.ios
 export const deviceTypes = {
 	ios: "ios",
 	android: "android"
