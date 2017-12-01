26eb49d61eef8e40a627c77c6861f0571a47df16
CheCm19 <che.cm@mail.com>
2017-12-01 03:20:59 -0800

    Implements create project place holder screens

---
+++ app/screens/createProject/CreateProjectDescription.js
@@ -0,0 +1,108 @@
+////////////////////////
+// Imports
+////////////////////////
+
+import React, { Component } from 'react';
+import {bindActionCreators, connect} from 'react-redux';
+import { 
+Text, 
+Dimensions,
+ScrollView,
+Image,
+StyleSheet,
+TouchableOpacity,
+View
+} from 'react-native';
+
+//////////////////////////////
+// Imports Common Files
+///////////////////////////////
+
+// Common styles
+import CommonStyles from "../../common/styles.js"
+
+////////////////////////
+// Actions
+////////////////////////
+
+import {accountLogout} from "./../../actions/account-actions.js";
+
+//////////////////////////////
+// Imports Custom Components
+///////////////////////////////
+
+import PrimaryButton from './../../components/PrimaryButton';
+
+////////////////////////
+// Constants
+////////////////////////
+const screenCenter                  = Dimensions.get('window').width/2;
+const PROJECT_TABBAR_ICON_IMAGE     = require('./../../assets/tabbar/settings_icon.png')
+
+class CreateProjectDescription extends Component {
+
+
+  tabBarOnPress(){
+    
+  }
+
+
+  ////////////////////////
+  // Navigation Options
+  ////////////////////////
+
+  static navigationOptions = {
+    headerRight: <TouchableOpacity><Text>Next</Text></TouchableOpacity>,
+  }
+
+  ////////////////////////
+  // Callbacks
+  ////////////////////////
+
+
+  ////////////////////////
+  // Screen UI
+  ////////////////////////
+
+  render() {
+    return (
+     <ScrollView style={CommonStyles.container}>
+     <Text> Create Project Description </Text>
+     </ScrollView>
+    );
+  }
+}
+
+////////////////////////
+// Screen Styles
+////////////////////////
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  tabBarIcon:{
+    width: 25,
+    height: 25
+  }
+});
+
+////////////////////////
+// Map to props
+////////////////////////
+
+const mapStateToProps = (state) => {
+  return {
+    isFetching: state.session.isFetching,
+    isErrored: state.session.isErrored
+  };
+}
+
+const mapDistpatchToProps = (dispatch) => {
+  return {
+    logout: () => dispatch(accountLogout())
+  };
+}
+
+export default connect(mapStateToProps, mapDistpatchToProps)(CreateProjectDescription);
---
+++ app/screens/createProject/CreateProjectImages.js
@@ -0,0 +1,107 @@
+////////////////////////
+// Imports
+////////////////////////
+
+import React, { Component } from 'react';
+import {bindActionCreators, connect} from 'react-redux';
+import { 
+Text, 
+Dimensions,
+ScrollView,
+Image,
+StyleSheet,
+TouchableOpacity,
+View
+} from 'react-native';
+
+//////////////////////////////
+// Imports Common Files
+///////////////////////////////
+
+// Common styles
+import CommonStyles from "../../common/styles.js"
+
+////////////////////////
+// Actions
+////////////////////////
+
+import {accountLogout} from "./../../actions/account-actions.js";
+
+//////////////////////////////
+// Imports Custom Components
+///////////////////////////////
+
+import PrimaryButton from './../../components/PrimaryButton';
+
+////////////////////////
+// Constants
+////////////////////////
+const screenCenter                  = Dimensions.get('window').width/2;
+
+class CreateProjectImages extends Component {
+
+
+  tabBarOnPress(){
+    
+  }
+
+
+  ////////////////////////
+  // Navigation Options
+  ////////////////////////
+
+  static navigationOptions = {
+    headerRight: <TouchableOpacity><Text>Next</Text></TouchableOpacity>,
+  }
+
+  ////////////////////////
+  // Callbacks
+  ////////////////////////
+
+
+  ////////////////////////
+  // Screen UI
+  ////////////////////////
+
+  render() {
+    return (
+     <ScrollView style={CommonStyles.container}>
+     <Text> Create Project Images</Text>
+     </ScrollView>
+    );
+  }
+}
+
+////////////////////////
+// Screen Styles
+////////////////////////
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  tabBarIcon:{
+    width: 25,
+    height: 25
+  }
+});
+
+////////////////////////
+// Map to props
+////////////////////////
+
+const mapStateToProps = (state) => {
+  return {
+    isFetching: state.session.isFetching,
+    isErrored: state.session.isErrored
+  };
+}
+
+const mapDistpatchToProps = (dispatch) => {
+  return {
+    logout: () => dispatch(accountLogout())
+  };
+}
+
+export default connect(mapStateToProps, mapDistpatchToProps)(CreateProjectImages);
