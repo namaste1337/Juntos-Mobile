3fd911c7ea62ebac0bad8d8dd8f86959b1c04973
CheCm19 <che.cm@mail.com>
2017-12-09 23:52:40 -0800

    Implements populateTempDescription action creator

--- app/actions/project-actions.js
+++ app/actions/project-actions.js
@@ -6,6 +6,17 @@
 
 import {NavigationActions} from "react-navigation";
 
+
+
+////////////////////////
+// Action Types
+////////////////////////
+
+// Available 
+export const ProjectActions = {
+	POPULATE_TEMP_DESCRIPTION: "POPULATE_TEMP_DESCRIPTION",
+}
+
 ////////////////////////
 // Action Creators
 ////////////////////////
@@ -13,5 +24,32 @@ import {NavigationActions} from "react-navigation";
 export function navigateToCreateProject(){
 
  return NavigationActions.navigate({routeName: "CreateProjectDescription"});
+
+}
+
+export function populateTempDescription(projectName, projectCoords, projectDescription){
+
+	let data = {}
+	data.projectName 		= projectName;
+	data.projectCoords 		= projectCoords;
+	data.projectDescription = projectDescription;
+ 
+	return {
+		type: POPULATE_TEMP_DESCRIPTION,
+		payload: data
+	}
+
+}
+
+export function populateTempDetails(projectDetailsTime, projectDetails){
+
+}
+
+export function populateTempImage(projectImageArray){
+
+
+}
+
+export function clearTempProject(){
 
 }
