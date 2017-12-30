27bf464b958c50fda0cbfd54195e9ad7fad992ea
CheCm19 <che.cm@mail.com>
2017-12-30 03:06:00 -0800

    Adjust padding for staticField

--- app/components/StaticField/styles.js
+++ app/components/StaticField/styles.js
@@ -18,10 +18,11 @@ let {height, width} = Dimensions.get('window');
 export default StyleSheet.create({
 
   text:{
-    marginLeft: 6,
     fontFamily: "Roboto-Light",
     color: "#929091",
-    fontSize: 14
+    fontSize: 14,
+    paddingVertical: 10,
+    paddingHorizontal: 8
   },
 
 });
