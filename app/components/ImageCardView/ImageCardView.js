51aa32e2cdc19c4e0d4d6df74db6b93ea6ec987b
CheCm19 <che.cm@mail.com>
2018-01-21 23:53:26 -0800

    Removes uneeded text elements to styles.js

--- app/components/ImageCardView/ImageCardView.js
+++ app/components/ImageCardView/ImageCardView.js
@@ -43,13 +43,13 @@ class  ImageCardView extends Component {
 
     return(
       <View>
-        <CardView>
-          <Image style={{width: width/4, height: width/4, paddingVertical: 5, paddingHorizontal: 5}} key={1} source={this.props.source}/>
-          <Text style={{fontFamily: "Roboto-Light", fontSize:10, paddingVertical: 5, paddingHorizontal: 5}}> This is an image...</Text>
+        <CardView style={this.props.style}>
+          <Image style={styles.imageCardView} source={this.props.source}/>
         </CardView>
       </View>
     )
+
   }
 };     
-// <View style={styles.imageCardWrapper}><Image style={{width: width/2, height: width/2, paddingVertical: 5, paddingHorizontal: 5}} key={1} source={image}/></View>
+
 export default ImageCardView;
