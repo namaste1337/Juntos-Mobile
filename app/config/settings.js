e58146f72659c0df7484dd3b15f613b5e04430d9
CheCm19 <che.cm@mail.com>
2018-02-09 19:07:41 -0800

    Adds constant to config/settings for remote assets servers

--- app/config/settings.js
+++ app/config/settings.js
@@ -1,3 +1,6 @@
 export default {
-	DEFAULT_INITIAL_VIEW: "Landing"
+	DEFAULT_INITIAL_VIEW: "Landing",
+	IMAGE_SERVER: "http://10.0.0.16:8080",
+	PROFILE_IMAGE_PATH: "/user_profile_images/",
+	PROJECT_IMAGE_PATH: "/project_images",
 }
