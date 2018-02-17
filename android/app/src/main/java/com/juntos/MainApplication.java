3ffaf83d33916ac6e50cd92858f6de0b68d65ae5
CheCm19 <che.cm@mail.com>
2018-02-17 02:35:33 -0800

    Links native libraries after react-native upgrade

--- android/app/src/main/java/com/juntos/MainApplication.java
+++ android/app/src/main/java/com/juntos/MainApplication.java
@@ -3,6 +3,9 @@ package com.juntos;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.beefe.picker.PickerViewPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new PickerViewPackage(),
+            new MapsPackage(),
+            new PickerPackage()
       );
     }
 
