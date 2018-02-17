1973b3b47b52e8e64dec127bcbb2b3dadf0cc47f
CheCm19 <che.cm@mail.com>
2018-02-17 02:22:59 -0800

    Updates AppRegistry to match updated name of app

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('juntosMobile', () => App);
+AppRegistry.registerComponent('Juntos', () => App);
