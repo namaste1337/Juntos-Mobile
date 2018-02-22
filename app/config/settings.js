a9848b5a9de2985084e3f82ea86e56819c50f0af
CheCm19 <che.cm@mail.com>
2018-02-21 18:00:54 -0800

    Updates the server path for images

--- app/config/settings.js
+++ app/config/settings.js
@@ -1,6 +1,5 @@
 export default {
 	DEFAULT_INITIAL_VIEW: "Landing",
 	IMAGE_SERVER: "http://10.0.0.16:8080",
-	PROFILE_IMAGE_PATH: "/user_profile_images/",
-	PROJECT_IMAGE_PATH: "/project_images",
+	IMAGE_PATH: "/images/",
 }
