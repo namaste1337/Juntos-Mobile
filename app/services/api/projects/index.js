262af2bfba89a41051af8fe7408025f2b6fabc53
CheCm19 <che.cm@mail.com>
2018-03-03 16:40:45 -0800

    Imlements a wrapper to project/index to avoid function name collisions, and implements service to fetch projects by location

--- app/services/api/projects/index.js
+++ app/services/api/projects/index.js
@@ -16,16 +16,29 @@ const PROJECTS_END_POINT = "/projects";
 // Functions
 ///////////////////////////
 
-// Handles getting all projects
-export function createProject(projectObject){
+export default {
 
-	return Services.fetchApi(PROJECTS_END_POINT, projectObject, Common.POST_METHOD);
+	// Handles getting all projects
+	createProject: function(projectObject){
 
-}
+		return Services.fetchApi(PROJECTS_END_POINT, projectObject, Common.POST_METHOD);
 
-// Handles getting all projects
-export function getAllProjects(){
+	},
+
+	// Handles getting all projects
+	getAllProjects: function(){
 
 	return Services.fetchApi(PROJECTS_END_POINT, {}, Common.GET_METHOD);
+
+	},
+
+	// Handles getting project by location
+	getProjectsByLocation: function(lat, lng, radius){
+
+		let query = "?geo=true&lat="+lat+"&lng="+lng+"&radius="+radius;
+
+		return Services.fetchApi(PROJECTS_END_POINT+query, {}, Common.GET_METHOD);
+
+	}
 
 }
