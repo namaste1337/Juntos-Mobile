59f00b3d287fdefc52d962b16519bc13623abe6e
CheCm19 <che.cm@mail.com>
2018-03-07 14:21:16 -0800

    Links react-native-deviceinfo for both android and ios

--- android/app/src/main/java/com/juntos/MainApplication.java
+++ android/app/src/main/java/com/juntos/MainApplication.java
@@ -3,6 +3,7 @@ package com.juntos;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.beefe.picker.PickerViewPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
             new PickerViewPackage(),
             new MapsPackage(),
             new PickerPackage()
