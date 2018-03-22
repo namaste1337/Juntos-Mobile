a76f04967017d93a6a5d6b02691f59861d8b6189
CheCm19 <che.cm@mail.com>
2018-03-21 23:15:56 -0700

    Adds react-native-app-settings module

--- android/app/src/main/java/com/juntos/MainApplication.java
+++ android/app/src/main/java/com/juntos/MainApplication.java
@@ -3,6 +3,7 @@ package com.juntos;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.krazylabs.OpenAppSettingsPackage;
 import com.aakashns.reactnativedialogs.ReactNativeDialogsPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.beefe.picker.PickerViewPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new OpenAppSettingsPackage(),
             new ReactNativeDialogsPackage(),
             new RNDeviceInfo(),
             new PickerViewPackage(),
