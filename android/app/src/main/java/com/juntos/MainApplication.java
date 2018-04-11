821202fbd5d703b57da046259ff0ba32fa1618ea
CheCm19 <che.cm@mail.com>
2018-04-11 00:06:24 -0700

    Updates react-native-svg to the latest version

--- android/app/src/main/java/com/juntos/MainApplication.java
+++ android/app/src/main/java/com/juntos/MainApplication.java
@@ -3,6 +3,7 @@ package com.juntos;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.krazylabs.OpenAppSettingsPackage;
 import com.aakashns.reactnativedialogs.ReactNativeDialogsPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new OpenAppSettingsPackage(),
             new ReactNativeDialogsPackage(),
             new RNDeviceInfo(),
