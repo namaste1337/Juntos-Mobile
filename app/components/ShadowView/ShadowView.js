845ec6b99ff9d4aefe1aaa4286b68e22b84dc285
CheCm19 <che.cm@mail.com>
2018-04-12 16:50:19 -0700

    Excludes android shadowview implementation, do to the lack of an elegant solution, shadows for android will be ignored

--- app/components/ShadowView/ShadowView.js
+++ app/components/ShadowView/ShadowView.js
@@ -5,12 +5,16 @@
 import React, { Component } from 'react';
 import {
   View,
+  StyleSheet
 } from 'react-native'
 
 ////////////////////////
 // Imports Common Files
 ////////////////////////
 
+// NOTE: Will be left in place for the android shadow implementation
+// see line 39.
+// import {deviceOS, deviceTypes} from "./../../common/device";
 
 ////////////////////////
 // Import Styles
@@ -18,23 +22,50 @@ import {
 
 import styles from "./styles";
 
+
+
 ////////////////////////
 // Componenet
 ////////////////////////
 
+
 class  ShadowView extends Component {
+
 
   ////////////////////////
   // Methods
   ////////////////////////
 
+  // TODO: The following is to allow seperate implementation
+  // for the Shadow view for android and iOS. For the moment
+  // we will ignore the Android implementation, do to the lack 
+  // of a graceful solution. The following snippet will be
+  // left in place to future proof the implementation.'
+
+  // _renderByOS = () =>{
+  //   if(deviceOS === deviceTypes.ios){
+  //     return(
+          // <View style={[styles.shadowViewWrapper, this.props.style]}>
+          //   {this.props.children}
+          // </View>
+  //     );
+  //   }else{
+  //     return (
+  //       <View>
+  //         {this.props.children}
+  //       </View>
+  //     );
+  //   }
+  // }
+
   render(){
 
     return(
-      <View onPress={this.props.onPress} style={[styles.shadowViewWrapper, this.props.style]}>
-        {this.props.children}
+      <View style={[styles.shadowViewWrapper, this.props.style]}>
+          {this.props.children}
       </View>
     )
   }
-};     
+};
+
 export default ShadowView;
